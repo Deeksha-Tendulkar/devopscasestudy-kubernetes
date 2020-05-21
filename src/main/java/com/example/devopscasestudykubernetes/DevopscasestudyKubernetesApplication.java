4a97fd3ac5044602d4ed715f3486efb25f54bad9
Deeksha-Tendulkar <deekshatend97@gmail.com>
2020-05-21 19:26:04 +0530

    updated

---
+++ src/main/java/com/example/devopscasestudykubernetes/DevopscasestudyKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopscasestudykubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopscasestudyKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopscasestudyKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopscasestudykubernetes/DevopscasestudyKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopscasestudykubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopscasestudyKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
